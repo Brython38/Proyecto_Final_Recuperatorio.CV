94677774f0760214405b9a5dd8616339500a19e8
Brian Martinez <brython3@gmail.com>
2023-10-09 20:07:08 -0300

    continuación del trabajo

---
+++ js/eventos.js
@@ -0,0 +1,15 @@
+document.getElementById("presentacion").addEventListener('click',function() {
+    document.getElementById("parrafo").style.display = 'block'
+});
+
+document.getElementById("educacion").addEventListener('click',function() {
+    document.getElementById("lista1").style.display = 'block'
+});
+
+document.getElementById("experiencia").addEventListener('click',function() {
+    document.getElementById("lista2").style.display = 'block'
+});
+
+document.getElementById("informacion").addEventListener('click',function() {
+    document.getElementById("datos").style.display = 'block'
+});
